9a1b6d793b4e6e33f42c58189728b14660a546ef
Khuchkharov25 <Khuchkharov25@gmail.com>
2024-02-04 23:44:05 +0900

    BRR: building express web server

---
+++ server.js
@@ -0,0 +1,35 @@
+console.log("Web serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require('http');
+
+
+// 1.  Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+// 2. Session code
+
+// 3.  Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+
+// 4. Rooting code
+
+app.get("/Hello", function(req, res){
+      res.end(`<h1 style="background: red">HELLO WORLD by Dostonbek</h1>`);
+});
+
+app.get("/gift", function(req, res){
+    res.end(`<h1 style="background: blue">Siz sovgalar bolimidasiz</h1>`);
+});
+
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function(){
+    console.log(`The server is running succesfully on ports: ${PORT}`);
+});

9961de1b035f62369438f5c6b1b94f2c82cd38c6
Khuchkharov25 <Khuchkharov25@gmail.com>
2024-02-05 13:50:34 +0900

    BRR: building express web server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 console.log("Web serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require('http');
 
@@ -19,12 +20,21 @@ app.set("view engine", "ejs");
 
 // 4. Rooting code
 
-app.get("/Hello", function(req, res){
-      res.end(`<h1 style="background: red">HELLO WORLD by Dostonbek</h1>`);
-});
+// app.get("/Hello", function(req, res){
+//       res.end(`<h1 style="background: red">HELLO WORLD by Dostonbek</h1>`);
+// });
 
-app.get("/gift", function(req, res){
-    res.end(`<h1 style="background: blue">Siz sovgalar bolimidasiz</h1>`);
+// app.get("/gift", function(req, res){
+//     res.end(`<h1 style="background: blue">Siz sovgalar bolimidasiz</h1>`);
+// });
+
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "succes"});
+})
+
+app.get("/", function(req, res){
+    res.render("harid");
 });
 
 

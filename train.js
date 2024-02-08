3fc45cf5ddf54080e40b4418cbeec86d67053e7a
Khuchkharov25 <Khuchkharov25@gmail.com>
2024-02-09 02:49:07 +0900

    feat: A task done

--- train.js
+++ train.js
@@ -1,2 +1,17 @@
 // A task 
-console.log("Train Area");
+console.log("Function letterida 'x' harfi nectaligini hisoblaymiz");
+
+
+let count = 0;
+
+function countLetter(a, b ) {
+    for( let i = 0; i <= a.length; i++) {
+        if(a.charAt(i) == b) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countLetter("engineer", "e"));
+ 

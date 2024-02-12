cee61c513595e36c2489870394e8d39e12a5ca19
Khuchkharov25 <Khuchkharov25@gmail.com>
2024-02-13 03:42:18 +0900

    feat: Reja loyihamizning yakuniy bosqichi

--- public/browser.js
+++ public/browser.js
@@ -31,3 +31,40 @@ document.getElementById("create-form").addEventListener("submit", function(e) {
     });
 
 });
+
+
+document.addEventListener("click", function(e) {
+
+  //delete operation
+  console.log(e.target);
+  if(e.target.classList.contains("delete-me")) {
+    if(confirm("Are You Sure?")) {
+    axios.post("/delete-item", {id: e.target.getAttribute("data-id")}).then((response) => {
+      console.log(response.data);
+      e.target.parentElement.parentElement.remove();
+    }).catch((err) => {
+      console.log("Iltimos Qaytadan Harakat Qiling");
+    });
+  }
+}
+  // edit operation
+  if(e.target.classList.contains("edit-me")) {
+    let userInput = prompt("Yangi Ozgartrishingizni kiriting", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+    );
+    if(userInput) {
+      axios.post("/edit-item", {id: e.target.getAttribute("data-id"), new_input: userInput,}).then((response) => {
+         console.log(response.data);
+         e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput;
+      }).catch((err) => {
+        console.log("Iltimos Qaytadan Harakat Qiling");
+      })
+    }
+  }
+});
+
+document.getElementById("clean-all").addEventListener("click", function(){
+  axios.post("/delete-all", {delete_all: true}).then((response) => {
+    alert(response.data.state);
+    document.location.reload();
+  })
+})
